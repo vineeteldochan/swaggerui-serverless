059672c1288f5d7b6ccae37312d9facd4f82e10c
Vineet Eldochan <vineeteldochan@outlook.com>
2019-11-17 12:25:50 +0100

    Added webpack to minify the dependencies and code

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+const path = require("path");
+const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  node: {
+    __dirname: true
+  },
+
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [__dirname],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    libraryTarget: "commonjs2",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development"
+    })
+  ]
+};
